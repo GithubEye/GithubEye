a38459187b6fb4b178e83ee530595cd3fca00711
splintersu <s.jinyue@gmail.com>
2016-10-15 12:13:38 +0800

    add get_readme.js

---
+++ getdata/get_readme.js
@@ -0,0 +1,23 @@
+module.exports = {
+get_readme : function(fullname , f)
+{
+	var options = {
+		host : 'raw.githubusercontent.com' , 
+		path : '/' + fullname + '/master/README.md' ,
+		method : 'GET' , 
+		headers : {'user-agent' : 'GooHub App'}
+	};
+
+	var result = '';
+
+	var https = require('https');
+	var request = https.get(options,
+		function(res){
+			console.log('Got response' + res.statusCode);
+			res.on('data' , function(data){
+				result += data;
+			});		
+		})
+	request.on('error' , function(e){console.log('got error' , e.message);});
+	request.on('close' , function(){f(result)})
+}
