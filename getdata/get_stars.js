6c74d8908752861dd9dd096f14c067cbac7ed50e
splintersu <s.jinyue@gmail.com>
2016-10-15 12:21:04 +0800

    get_stars.js

---
+++ getdata/get_stars.js
@@ -0,0 +1,30 @@
+module.exports = {
+get_readme : function(fullname , f)
+{
+	var options = {
+		host : 'api.github.com' , 
+		path : '/repos/' + fullname + '/stargazers' ,
+		method : 'GET' , 
+		headers : {'user-agent' : 'GooHub App'}
+	};
+
+	var result = 0
+
+	var https = require('https');
+	var request = https.get(options,
+		function(res){
+			var full_respond = '';
+			console.log('Got response' + res.statusCode);
+
+			res.on('data' , function(data){
+				full_respond += data;
+			});
+
+			res.on('end' , function(){
+				var tmp = JSON.parse(full_respond);
+				result = tmp.length
+			});
+		})
+	request.on('error' , function(e){console.log('got error' , e.message);});
+	request.on('close' , function(){f(result)})
+}
